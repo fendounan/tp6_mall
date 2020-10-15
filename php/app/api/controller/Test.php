ce53cf61c282d7df69d8aa61a01065b75870afdb
superAdmin <309712560@qq.com>
2020-10-15 21:58:44 +0800

    test for ab

--- php/app/api/controller/Test.php
+++ php/app/api/controller/Test.php
@@ -13,12 +13,13 @@ use app\common\business\Sms as SmsBus;
 use app\api\validate\User as UserValidate;
 use app\common\business\User;
 use app\common\lib\Snowflake;
+use think\facade\Db;
 
 class Test extends BaseController
 {
     public function test()
     {
-        echo hash('sha256',1);
+        // echo hash('sha256',1);
 
         // 异常处理 场景1
         // throw new \think\exception\HttpException(401, '找不到相应的数据');
@@ -27,8 +28,13 @@ class Test extends BaseController
         // echo $aaa;
 
         // 雪花算法生成唯一id workid 机器id最大1023 源码中定义
-        $workId = rand(1,1023);
-        $id = Snowflake::getInstance()->setWorkId($workId)->nextId();
-        echo $id;
+        $workId = rand(1, 1023);
+        $serial = Snowflake::getInstance()->setWorkId($workId)->nextId();
+        $arr = input('post');
+        $arr = json_encode($arr);
+        $data = ['serial' => $serial,'case_name'=>$arr, 'time_create' => date('Y-m-d H:i:s')];
+        Db::name('tb_log_api_copy1')->insert($data);
+
+
     }
 }
